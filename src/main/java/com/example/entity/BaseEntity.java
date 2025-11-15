b780951def413183df9cd1ec65ceab63acf6d471
202204170031 cjy <amianmmm@163.com>
2025-11-15 12:18:15 +0800

    第八章-进度3-代码生成，并通过测试类测试获取数据库sys_user表信息

---
+++ src/main/java/com/example/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.example.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    private LocalDateTime updated;
+    private Integer statu;
+}
