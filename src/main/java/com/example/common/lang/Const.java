f749213945f02762c02685d4a1fbe77fc72cf2b0
202204170031 cjy <amianmmm@163.com>
2025-11-21 16:48:50 +0800

    第九章-Spring Security整合Jwt,图片验证码与跨域问题解决

---
+++ src/main/java/com/example/common/lang/Const.java
@@ -0,0 +1,5 @@
+package com.example.common.lang;
+
+public class Const {
+    public static final String CAPTCHA_KEY = "captcha";
+}
---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //白名单
+    private static final String[] URL_WHITELIST = {
+            "/login",
+            "/logout",
+            "/captcha",
+            "/favicon.ico",
+    };
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                //登录配置
+                .formLogin()
+
+                //禁用session
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                //配置拦截规则
+                .and()
+                .authorizeRequests()
+                .antMatchers(URL_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+
+                //异常处理
+
+                //配置自定义过滤器
+
+        ;
+    }
+}
