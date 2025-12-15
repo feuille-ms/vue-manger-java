dd45a12fb655a5479a3327f47b5d11d2de366baf
202204170031 cjy <amianmmm@163.com>
2025-12-16 00:50:20 +0800

    十四章-进度1：用户管理功能实现

--- src/main/java/com/example/controller/SysUserController.java
+++ src/main/java/com/example/controller/SysUserController.java
@@ -1,9 +1,25 @@
 package com.example.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.common.lang.Const;
+import com.example.common.lang.Result;
+import com.example.entity.SysRole;
+import com.example.entity.SysUser;
+import com.example.entity.SysUserRole;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +30,101 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2025-11-15
  */
 @RestController
-@RequestMapping("/sys-user")
+@RequestMapping("/sys/user")
 public class SysUserController extends BaseController {
+    private final PasswordEncoder passwordEncoder;
 
+    public SysUserController(PasswordEncoder passwordEncoder) {
+        super();
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/info/{id}")
+    @PreAuthorize("hasAuthority('sys:user:list')")
+    public Result info(@PathVariable("id") Long id) {
+        SysUser sysUser = sysUserService.getById(id);
+        Assert.notNull(sysUser, "找不到该管理员");
+        List<SysRole> roles = sysRoleService.listRolesByUserId(id);
+        sysUser.setSysRoles(roles);
+        return Result.succ(sysUser);
+    }
+
+    @GetMapping("/list")
+    @PreAuthorize("hasAuthority('sys:user:list')")
+    public Result list(String username){
+        Page<SysUser> pageDate = sysUserService.page(getPage(),
+                new QueryWrapper<SysUser>().like(StringUtils.isNotBlank(username), "username", username));
+
+        pageDate.getRecords().forEach(u -> {
+            u.setSysRoles(sysRoleService.listRolesByUserId(u.getId()));
+        });
+        return Result.succ(pageDate);
+    }
+
+    @PostMapping("/save")
+    @PreAuthorize("hasAuthority('sys:user:save')")
+    public Result save(@Valid @RequestBody SysUser sysUser) {
+        sysUser.setCreated(LocalDateTime.now().now());
+        if (sysUser.getStatu() == 1){
+            sysUser.setStatu(Const.STATUS_ON);
+        }else {
+            sysUser.setStatu(Const.STATUS_OFF);
+        }
+        String password = passwordEncoder.encode(Const.DEFAULT_PASSWORD);
+        sysUser.setPassword(password);
+        sysUser.setAvatar(Const.DEFAULT_AVATAR);
+        sysUserService.save(sysUser);
+        return Result.succ(sysUser);
+    }
+
+    @PostMapping("/update")
+    @PreAuthorize("hasAuthority('sys:user:update')")
+    public Result update(@Valid @RequestBody SysUser sysUser) {
+        sysUser.setUpdated(LocalDateTime.now());
+        sysUserService.updateById(sysUser);
+        return Result.succ(sysUser);
+    }
+
+    @PostMapping("/delete")
+    @PreAuthorize("hasAuthority('sys:user:delete')")
+    public Result delete(@RequestBody Long[] ids){
+        sysUserService.removeByIds(Arrays.asList(ids));
+        sysUserRoleService.remove(new QueryWrapper<SysUserRole>().in("user_id", ids));
+        return Result.succ("");
+    }
+
+    @Transactional
+    @PostMapping("/role/{userId}")
+    @PreAuthorize("hasAuthority('sys:user:role')")
+    public Result rolePerm(@PathVariable("userId") Long userId, @RequestBody Long[] roleIds) {
+        List<SysUserRole> userRoles = new ArrayList<>();
+
+        Arrays.stream(roleIds).forEach(r -> {
+            SysUserRole sysUserRole = new SysUserRole();
+            sysUserRole.setUserId(userId);
+            sysUserRole.setRoleId(r);
+
+            userRoles.add(sysUserRole);
+        });
+
+        sysUserRoleService.remove(new QueryWrapper<SysUserRole>().eq("user_id", userId));
+        sysUserRoleService.saveBatch(userRoles);
+
+        //删除缓存
+        SysUser sysUser = sysUserService.getById(userId);
+        sysUserService.clearUserAuthorityInfo(sysUser.getUsername());
+
+        return Result.succ("");
+    }
+
+    @PostMapping("/repass")
+    @PreAuthorize("hasAuthority('sys:user:repass')")
+    public Result repass(@RequestBody Long userId) {
+        SysUser sysUser = sysUserService.getById(userId);
+        sysUser.setPassword(passwordEncoder.encode(Const.DEFAULT_PASSWORD));
+        sysUser.setUpdated(LocalDateTime.now());
+
+        sysUserService.updateById(sysUser);
+        return Result.succ("");
+    }
 }
--- src/main/java/com/example/service/SysRoleService.java
+++ src/main/java/com/example/service/SysRoleService.java
@@ -3,6 +3,8 @@ package com.example.service;
 import com.example.entity.SysRole;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2025-11-15
  */
 public interface SysRoleService extends IService<SysRole> {
-
+    List<SysRole> listRolesByUserId(Long userId);
 }
